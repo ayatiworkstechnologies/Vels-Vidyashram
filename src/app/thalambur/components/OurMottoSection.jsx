7f5d3ba3d68ecd2cd8d461c5dcf06cfe103c8312
srirmaya333 <srirmaya333@gmail.com>
2026-06-24 10:25:33 +0530

    missed image

--- src/app/thalambur/components/OurMottoSection.jsx
+++ src/app/thalambur/components/OurMottoSection.jsx
@@ -12,7 +12,7 @@ export default function OurMottoSection() {
     {
       title: "Our Group",
       text: "A trusted group of institutions dedicated to academic excellence and values. Shaping young minds through quality education and innovation.",
-      image: "/thalambur/our-group-1.png",
+      image: "/thalambur/our.png",
       showButton: true,
       link: "/thalambur/our-group",
     },
